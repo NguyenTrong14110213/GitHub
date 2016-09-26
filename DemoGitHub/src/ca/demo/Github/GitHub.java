bf4c8a462795304337223cc1d2d267606d8b1b03
USER <USER@USER-PC>
2016-09-26 23:43:12 +0700

    Test github

---
+++ DemoGitHub/src/ca/demo/Github/GitHub.java
@@ -0,0 +1,8 @@
+package ca.demo.Github;
+
+public class GitHub {
+	int data[]={0,4,42};
+	public static void display(){
+	System.out.println("Test demo github");
+	}
+}
---
+++ DemoGitHub/src/ca/demo/Github/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.Github;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GitHub  data=new GitHub();
+		displayGreeting();
+		data.display();
+	}
+	private static void displayGreeting()
+	{
+		System.out.println("Nhóm 15");
+		System.out.println("demo github");
+	}
+
+}
